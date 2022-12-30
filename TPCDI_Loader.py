e99eab5bf2105414cdd967ab541a96c30be1e96b
zivkovicbogdana <bogdanaz207@gmail.com>
2022-12-30 12:46:57 +0100

    Fact watches load

--- TPCDI_Loader.py
+++ TPCDI_Loader.py
@@ -753,6 +753,35 @@ class TPCDI_Loader():
     os.system(cmd)
     print('Done.')
 
+  def load_watches(self):
+    print('Loading FactWatches...')
+    insert_watches_query = """
+    INSERT INTO FactWatches (SK_CustomerID, SK_SecurityID, SK_DateID_DatePlaced, SK_DateID_DateRemoved, BatchID)
+    WITH Active AS (
+        SELECT W_C_ID, W_S_SYMB, DC.SK_CustomerID, DS.SK_SecurityID, DD.SK_DateID
+        FROM S_Watches W
+            INNER JOIN DimCustomer DC ON (W.W_C_ID = DC.CUSTOMERID)
+            INNER JOIN DimSecurity DS ON (W.W_S_SYMB = DS.Symbol)
+            INNER JOIN DimDate DD ON (TO_CHAR(W.W_DTS, 'YYYY-MM-DD') = TO_CHAR(DD.DateValue, 'YYYY-MM-DD')) 
+        WHERE W.W_ACTION = 'ACTV'
+    ),
+    Cancelled AS (
+        SELECT W_C_ID, W_S_SYMB, DD.SK_DateID
+        FROM S_Watches W 
+            INNER JOIN DimDate DD ON (TO_CHAR(W.W_DTS, 'YYYY-MM-DD') = TO_CHAR(DD.DateValue, 'YYYY-MM-DD')) 
+        WHERE W.W_ACTION = 'CNCL'
+    )
+    SELECT A.SK_CustomerID, A.SK_SecurityID, A.SK_DateID, C.SK_DateID, {0} BatchID
+    FROM Active A LEFT OUTER JOIN Cancelled C ON (A.W_C_ID = C.W_C_ID AND A.W_S_SYMB = C.W_S_SYMB)
+    """.format(self.batch_number)
+    with oracledb.connect(
+            user=self.oracle_user, password=self.oracle_pwd,
+            dsn=self.oracle_host + '/' + self.oracle_db) as connection:
+      with connection.cursor() as cursor:
+        cursor.execute(insert_watches_query)
+      connection.commit()
+    print('Done.')
+
   def load_staging_prospect(self):
     """
     Load rows in Prospect.csv into S_Prospect table in staging database.
